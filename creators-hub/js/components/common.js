3f36034b169b3c0d07d1495c8f53db7eea59672f
ronniem339 <ronniem339@gmail.com>
2025-06-15 19:55:52 +0100

    Update common.js

--- creators-hub/js/components/common.js
+++ creators-hub/js/components/common.js
@@ -329,8 +329,29 @@ const CopyButton = ({ textToCopy }) => {
     );
 };
 
+window.CanvaModal = ({ canvaUrl, onClose }) => {
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-90 z-[100] flex flex-col p-4">
+            <div className="flex justify-end mb-4">
+                <button onClick={onClose} className="text-white text-3xl font-bold leading-none">&times;</button>
+            </div>
+            <div className="flex-grow bg-white rounded-lg">
+                <iframe
+                    src={canvaUrl}
+                    className="w-full h-full border-0 rounded-lg"
+                    title="Canva Editor"
+                ></iframe>
+            </div>
+        </div>
+    );
+};
+
 // EXPOSE NEW COMPONENTS GLOBALLY
 window.CopyButton = CopyButton;
-
-// Removed window.TaskItem as it's replaced by Accordion in VideoWorkspace.js
-// Removed window.TaskItem = TaskItem;
