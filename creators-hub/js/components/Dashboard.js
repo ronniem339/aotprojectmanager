7be7edd61fdfede83fe289855ab25c82ebe91c9f
ronniem339 <ronniem339@gmail.com>
2025-06-16 16:23:46 +0100

    Update Dashboard.js

--- creators-hub/js/components/Dashboard.js
+++ creators-hub/js/components/Dashboard.js
@@ -1,6 +1,6 @@
 // js/components/Dashboard.js
 
-window.Dashboard = ({ userId, onSelectProject, onShowSettings, onShowProjectSelection, onShowDeleteConfirm, onShowKnowledgeBases }) => { // Added onShowKnowledgeBases just in case, though not directly used in header per new order
+window.Dashboard = ({ userId, onSelectProject, onShowSettings, onShowProjectSelection, onShowDeleteConfirm }) => { // Removed onShowKnowledgeBases from props as it's not a direct header item
     const { useState, useEffect, useRef, useMemo } = React;
     const [projects, setProjects] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -238,7 +238,7 @@ window.Dashboard = ({ userId, onSelectProject, onShowSettings, onShowProjectSele
                                             className="absolute top-2 right-2 p-1.5 bg-red-800/70 text-white rounded-full opacity-0 group-hover:opacity-100 hover:bg-red-700 transition-opacity"
                                             aria-label="Delete project"
                                         >
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" />
                                             </svg>
                                         </button>
