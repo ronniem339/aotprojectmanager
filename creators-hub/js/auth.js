4e22d0688e414d6b6eb82dd713eaf6da72c60caa
ronniem339 <ronniem339@gmail.com>
2025-06-18 08:27:29 +0100

    Update auth.js

--- creators-hub/js/auth.js
+++ creators-hub/js/auth.js
@@ -1,9 +1,1 @@
-// This script initializes Firebase and makes the auth and db objects
-// globally available for other scripts to use.
 
-// Initialize Firebase using the config from config.js
-firebase.initializeApp(window.CREATOR_HUB_CONFIG.FIREBASE_CONFIG);
-
-// Create global instances for Firebase services
-window.auth = firebase.auth();
-window.db = firebase.firestore();
