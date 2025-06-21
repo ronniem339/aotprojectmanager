213fd3d01fc43d30a927aea2e4ced2d3662fe77c
ronniem339 <ronniem339@gmail.com>
2025-06-21 23:08:11 +0100

    Update fetch-place-details.js

--- netlify/functions/fetch-place-details.js
+++ netlify/functions/fetch-place-details.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
   const { place_id } = event.queryStringParameters;
 
   // Securely access your API key from a Netlify environment variable
-  const apiKey = process.env.Maps_API_KEY_SERVER;
+  const apiKey = process.env.VITE_Maps_API_KEY;
 
   if (!place_id) {
     return { statusCode: 400, body: 'Missing place_id parameter' };
