e5b64dc44ddb01e3c26861d0e55e2970f76bb45d
ronniem339 <ronniem339@gmail.com>
2025-06-25 23:05:27 +0100

    Create MockLocationSearchInput.js

---
+++ creators-hub/js/components/ui/MockLocationSearchInput.js
@@ -0,0 +1,3 @@
+window.MockLocationSearchInput = () => {
+    return <p className="text-sm text-amber-400 p-3 bg-amber-900/50 rounded-lg">Please enter a valid Google Maps API Key in the settings to enable location search.</p>;
+};
