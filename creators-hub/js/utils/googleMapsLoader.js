67e4f6842bc8f7c4efcbe0f0ac82b9ec11846ccd
ronniem339 <ronniem339@gmail.com>
2025-06-25 23:07:10 +0100

    Create googleMapsLoader.js

---
+++ creators-hub/js/utils/googleMapsLoader.js
@@ -0,0 +1,24 @@
+window.loadGoogleMapsScript = (apiKey, callback) => {
+    if (window.google?.maps?.places) {
+        if (callback) callback();
+        return;
+    }
+    const existingScript = document.getElementById('googleMaps');
+    if (existingScript) {
+        const checkGoogle = setInterval(() => {
+            if (window.google?.maps?.places) {
+                clearInterval(checkGoogle);
+                if (callback) callback();
+            }
+        }, 100);
+        return;
+    }
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+    script.id = 'googleMaps';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    script.onload = () => { if (callback) callback() };
+    script.onerror = () => console.error("Google Maps script failed to load.");
+};
