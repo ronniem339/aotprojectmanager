caf808851cd4a07bd52af18e47acbd0261795237
ronniem339 <ronniem339@gmail.com>
2025-06-25 23:06:30 +0100

    Create useDebounce.js

---
+++ creators-hub/js/hooks/useDebounce.js
@@ -0,0 +1,10 @@
+const { useState, useEffect } = React;
+
+window.useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(() => {
+        const handler = setTimeout(() => { setDebouncedValue(value); }, delay);
+        return () => { clearTimeout(handler); };
+    }, [value, delay]);
+    return debouncedValue;
+};
