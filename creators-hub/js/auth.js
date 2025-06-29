111414c38284aa206779669e518766192a45b4d4
ronniem339 <ronniem339@gmail.com>
2025-06-29 08:49:57 +0100

    Update auth.js

--- creators-hub/js/auth.js
+++ creators-hub/js/auth.js
@@ -1,13 +1,1 @@
-// creators-hub/js/auth.js
 
-// Initialize Firebase
-try {
-    if (firebase.apps.length === 0) {
-        firebase.initializeApp(window.CREATOR_HUB_CONFIG.FIREBASE_CONFIG);
-    }
-} catch (e) {
-    console.error("Firebase initialization error:", e);
-}
-
-const auth = firebase.auth();
-const db = firebase.firestore();
