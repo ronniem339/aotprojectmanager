ae002bdb60252f17a08a7c413010636a0232c4a6
ronniem339 <ronniem339@gmail.com>
2025-07-05 14:53:12 +0100

    Update createInitialBlueprintAI.js

--- creators-hub/js/utils/ai/scriptingV2/createInitialBlueprintAI.js
+++ creators-hub/js/utils/ai/scriptingV2/createInitialBlueprintAI.js
@@ -9,7 +9,7 @@ const simpleUUID = () => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
 );
 
-window.createInitialBlueprintAI = async ({ initialThoughts, video, project, settings }) => {
+window.aiUtils.createInitialBlueprintAI = async ({ initialThoughts, video, project, settings }) => { // CHANGED: Attached to window.aiUtils
     console.log("Generating Initial Blueprint with AI...");
 
     // --- Input Validation ---
